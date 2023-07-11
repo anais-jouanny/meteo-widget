3bbb6e3b72b0a05466e8f0fb0710671448b881da
Anaïs Jouanny <anais.jouanny@adevinta.com>
2023-07-11 17:48:16 +0200

    delete city done

--- pages/delete-city.tsx
+++ pages/delete-city.tsx
@@ -1,8 +1,32 @@
 import { useContext } from "react";
 import { AppContext } from "../context/context";
+import deleteCityStyles from "../modules/DeleteCity.module.css";
+import DeleteCityInput from "../components/DeleteCity/DeleteCityInput";
 
 const DeleteCity = () => {
-  const { cities } = useContext(AppContext);
+  const { cities, setCities } = useContext(AppContext);
+
+  const handleOnClick = (cityToDelete: string) => {
+    const updatedCities = cities.filter((city) => city !== cityToDelete);
+    setCities(updatedCities);
+  };
+
+  return (
+    <div>
+      <h2 className={deleteCityStyles.title}>Voici la liste de vos villes :</h2>
+
+      <ul className={deleteCityStyles.list}>
+        {cities.map((city) => {
+          return (
+            <li className={deleteCityStyles.listItem} key={city}>
+              {city}
+              <DeleteCityInput city={city} onClick={handleOnClick} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default DeleteCity;
